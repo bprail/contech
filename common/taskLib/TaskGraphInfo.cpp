2c53748cc4e51adf9ec0578d6d5d27d0cf609ae1
Brian Paul Railing <brailing@ristorante.cc.gatech.edu>
2014-04-06 21:58:22 -0400

    Extend stub APIs in TaskGraphInfo
    Continuing to prepare APIs based on usage in middle layer

--- common/taskLib/TaskGraphInfo.cpp
+++ common/taskLib/TaskGraphInfo.cpp
@@ -6,3 +6,18 @@ TaskGraphInfo::TaskGraphInfo(ct_file*)
 {
 
 }
+
+TaskGraphInfo::TaskGraphInfo()
+{
+
+}
+
+void TaskGraphInfo::addRawBasicBlockInfo(uint bbid, uint lineNum, uint numMemOps, string function)
+{
+
+}
+
+void TaskGraphInfo::writeTaskGraphInfo(ct_file*)
+{
+
+}
--- common/taskLib/TaskGraphInfo.hpp
+++ common/taskLib/TaskGraphInfo.hpp
@@ -12,15 +12,47 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <string>
 #include <inttypes.h>
 
 using namespace std;
 namespace contech {
 
+class BasicBlockInfo
+{
+public:
+    uint lineNumber;
+    uint functionNumber;
+    uint fileName;
+    vector <uint> typeOfMemOps;
+};
+
+class FunctionInfo
+{
+public:
+    string functionName;
+    vector <uint> typeOfArguments;
+    uint returnType;
+};
+
+class TypeInfo
+{
+    string typeName;
+    size_t sizeOfType;
+};
+
 class TaskGraphInfo
 {
 public:
+    map <uint, BasicBlockInfo> bbInfo;
+    map <uint, TypeInfo> tyInfo;
+    map <uint, FunctionInfo> funInfo;
+    map <uint, string> fileName;
     TaskGraphInfo(ct_file*);
+    TaskGraphInfo();
+    
+    void addRawBasicBlockInfo(uint bbid, uint lineNum, uint numMemOps, string function);
+    void writeTaskGraphInfo(ct_file*);
 };
 
 }
