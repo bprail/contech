5b1cefdacfa052cecd1e54d471a0970d3e3705b9
Brian Railing <brian.railing@cc.gatech.edu>
2016-01-18 09:14:43 -0500

    TaskLib - Update ct_file for style and revise read / write semantics
    Ct_file read / write will process all of the bytes, rather than a single call.

--- common/taskLib/ct_file.c
+++ common/taskLib/ct_file.c
@@ -28,7 +28,8 @@ struct _ct_file
     pthread_mutex_t fileLock;
 };
 
-ct_file* create_ct_file_blank(){
+ct_file* create_ct_file_blank()
+{
     ct_file* newHandle = (ct_file*) calloc(1,sizeof(ct_file));
     
     pthread_mutex_init(&newHandle->fileLock, NULL);
@@ -36,73 +37,92 @@ ct_file* create_ct_file_blank(){
     return newHandle;
 }
 
-ct_file* create_ct_file_w(const char* fileName,bool compressed){
+ct_file* create_ct_file_w(const char* fileName,bool compressed)
+{
     ct_file* newHandle = create_ct_file_blank();
     FILE* tempFileHandle = fopen (fileName,"wb");
 
-    if(compressed){
+    if (compressed)
+    {
         newHandle->compressedHandle = gzdopen (fileno(tempFileHandle), "wb");
-    } else {
+    } 
+    else 
+    {
         newHandle->uncompressedHandle = tempFileHandle;
     }
     return newHandle;
 }
 
-ct_file* create_ct_file_r(const char* fileName){
+ct_file* create_ct_file_r(const char* fileName)
+{
     ct_file* newHandle = create_ct_file_blank();
     FILE* tempFileHandle = fopen (fileName,"rb");
     
     //read whether file is compressed or uncompressed.
     bool compressed;
     unsigned char zipTest[2];
-    if(tempFileHandle == NULL){
+    if (tempFileHandle == NULL)
+    {
         return NULL;
     }
     
     // read the first two bytes and check if they are equal to 0x1f8b.
     // false positive ARE possible (though unlikely) with this method but not false negatives
     fread(zipTest,sizeof(char),2,tempFileHandle);
-    if(zipTest[0] == 0x1f && zipTest[1] == 0x8b){ // these are "magic" numbers stored in the bytes of gzip'ed files
+    if (zipTest[0] == 0x1f && zipTest[1] == 0x8b)
+    { // these are "magic" numbers stored in the bytes of gzip'ed files
         compressed = true;
-    } else {
+    } 
+    else 
+    {
         compressed = false;
     }
     rewind(tempFileHandle); //rewind handle to to the beginning, gzip errors otherwise
-    if(compressed){
+    if(compressed)
+    {
         newHandle->compressedHandle = gzdopen(fileno(tempFileHandle), "rb");
-    } else {
+    } 
+    else 
+    {
         newHandle->uncompressedHandle = tempFileHandle;
     }
     return newHandle;
 }
 
 
-ct_file* create_ct_file_from_handle(FILE* existingHandle){
+ct_file* create_ct_file_from_handle(FILE* existingHandle)
+{
     ct_file* newHandle = create_ct_file_blank();
     newHandle->uncompressedHandle = existingHandle;
     return newHandle;
 }
 
-gzFile getCompressedHandle(ct_file* handle){
+gzFile getCompressedHandle(ct_file* handle)
+{
     return handle->compressedHandle;    
 }
 
-FILE* getUncompressedHandle(ct_file* handle){
+FILE* getUncompressedHandle(ct_file* handle)
+{
     return handle->uncompressedHandle;
 }
 
-bool isClosed(ct_file* handle){
+bool isClosed(ct_file* handle)
+{
     return getCompressedHandle(handle) == NULL && getUncompressedHandle(handle) == NULL;
 }
 
-bool isCompressed(ct_file* handle){
-    if(getCompressedHandle(handle) == NULL){
+bool isCompressed(ct_file* handle)
+{
+    if(getCompressedHandle(handle) == NULL)
+    {
         return false;
     }
     return true;
 }
 
-void close_ct_file(ct_file* handle){
+void close_ct_file(ct_file* handle)
+{
     if(isClosed(handle) == false){ 
         if(isCompressed(handle)){
             gzclose(getCompressedHandle(handle));
@@ -114,12 +134,24 @@ void close_ct_file(ct_file* handle){
     }
 }
 
-size_t ct_read(void * ptr, size_t size, ct_file* handle){
+size_t ct_read(void * ptr, size_t size, ct_file* handle)
+{
     size_t read = 0;
-    if(isCompressed(handle)){
-        read = gzread (getCompressedHandle(handle), ptr, size);
-    } else {
-        read = fread(ptr,1,size,getUncompressedHandle(handle));
+    if (isCompressed(handle)) 
+    {
+        do {
+            size_t r = gzread (getCompressedHandle(handle), ptr + read, size - read);
+            read += r;
+            if (r == 0 && ct_eof(handle)) break;
+        } while (read < size);
+    } 
+    else 
+    {
+        do {
+            size_t r = fread(ptr + read, 1, size - read, getUncompressedHandle(handle));
+            read += r * 1; // fread returns the number of items read
+            if (r == 0 && ct_eof(handle)) break;
+        } while (read < size);
     }
     
     if (scanTraceFlag != 0)
@@ -131,33 +163,51 @@ size_t ct_read(void * ptr, size_t size, ct_file* handle){
 }
 
 
-size_t ct_write(const void * ptr, size_t size, ct_file* handle){
+size_t ct_write(const void * ptr, size_t size, ct_file* handle)
+{
     size_t written = 0;
-    if(isCompressed(handle)){
-        written = gzwrite (getCompressedHandle(handle), ptr, size);
-    } else {
-        written = fwrite(ptr,1,size,getUncompressedHandle(handle));
+    if( isCompressed(handle))
+    {
+        do {
+            size_t w = gzwrite (getCompressedHandle(handle), ptr + written, size - written);
+            written += w;
+        } while (written < size);
+    } 
+    else 
+    {
+        do {
+            size_t w = fwrite(ptr + written, 1, size - written, getUncompressedHandle(handle));
+            written += w * 1;
+        } while (written < size);
     }
     return written;
 }
 
-int ct_eof(ct_file* handle){
+int ct_eof(ct_file* handle)
+{
     int result;
-    if(isCompressed(handle)){
+    if (isCompressed(handle))
+    {
         result = gzeof (getCompressedHandle(handle));
-    } else {
+    } 
+    else 
+    {
         result = feof (getUncompressedHandle(handle));
     }
     return result;
 }
 
-int ct_seek( ct_file* handle, unsigned long long offset){
+int ct_seek( ct_file* handle, unsigned long long offset)
+{
     int result;
-    if(isCompressed(handle)){
+    if(isCompressed(handle))
+    {
         result = gzseek (getCompressedHandle(handle), offset, SEEK_SET);
         // success for gzseek is defined as offset, whereas fseek returns 0
         if (result == offset) return 0;
-    } else {
+    } 
+    else 
+    {
         result = fseek (getUncompressedHandle(handle), offset, SEEK_SET );
     }
     return result;
@@ -167,22 +217,31 @@ void ct_rewind(ct_file* handle){
     ct_seek(handle, 0);
 }
 
-int ct_flush(ct_file* handle){
+int ct_flush(ct_file* handle)
+{
     int result;
-    if(isCompressed(handle)){
+    
+    if (isCompressed(handle))
+    {
         result = gzflush (getCompressedHandle(handle), Z_FULL_FLUSH);
-    } else {
+    } 
+    else 
+    {
         result = fflush (getUncompressedHandle(handle));
     }
+    
     return result;
 }
 
 long ct_tell(ct_file* handle)
 {
     long result = 0;
-    if(isCompressed(handle)){
+    if (isCompressed(handle))
+    {
         result = gztell (getCompressedHandle(handle));
-    } else {
+    } 
+    else 
+    {
         result = ftell (getUncompressedHandle(handle));
     }
     return result;
