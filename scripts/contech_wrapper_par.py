9d93ad39520eab84fb6f5c831de2f7cada0bf99d
Brian Railing <bpr@orthanc.csd.cs.cmu.edu>
2016-03-16 14:22:32 -0400

    Compiler Script - Library support and environment variables
    Update script to remove libraries from link as the object files are extracted
    and instrumented together.
    
    The CONTECH_LLVM_HOME environment variable is no longer required.

--- scripts/contech_wrapper_par.py
+++ scripts/contech_wrapper_par.py
@@ -30,9 +30,10 @@ def main(isCpp = False, markOnly = False, minimal = False, hammer = False):
             CLANGPP = CLANG + "++"
             OPT = CONTECH_LLVM_HOME + "/bin/opt"
         else:
-            ""
-        #LLVMHAMMER = CONTECH_HOME + "/llvm_fe_3.2/build/Release+Asserts/lib/LLVMHammer.so"
-        #RUNTIME = CONTECH_HOME + "/common/runtime/libct_runtime.a"
+            LLVMCONTECH = CONTECH_HOME + "/llvm-contech/lib/LLVMContech.so"
+            CLANG = "clang"
+            CLANGPP = "clang++"
+            OPT = "opt"
         if markOnly:
             # Use the .o file so that LLVM does not optimize away the marker calls
             RUNTIME = CONTECH_HOME + "/common/runtime/ct_runtime.o"
@@ -68,7 +69,7 @@ def main(isCpp = False, markOnly = False, minimal = False, hammer = False):
     if ARM == True:
         CFLAGS="--verbose -pthread"
     else:
-        CFLAGS="-flto --verbose -pthread"
+        CFLAGS="--verbose -pthread"
 
     # Choose correct compiler
     if isCpp:
@@ -189,42 +190,16 @@ def main(isCpp = False, markOnly = False, minimal = False, hammer = False):
         if newobj[-2:] != ".o":
             newobj = newobj + ".o"
         
-        if True:
-            pcall(["gcc", CFLAGS, "-S", "-fplugin=" + CONTECH_LLVM_HOME + "/lib/dragonegg.so" , "-fplugin-arg-dragonegg-emit-ir", cfile, "-o", newobj])
-            ofiles = ofiles + " " + newobj
-        
-        else:
-            # -fplugin=$CONTECH_LLVM_HOME/lib64/dragonegg.so -S -fplugin-arg-dragonegg-emit-ir
-            pcall(["gcc", CFLAGS, "-S", "-fplugin=" + CONTECH_LLVM_HOME + "/lib/dragonegg.so" , "-fplugin-arg-dragonegg-emit-ir", cfile, "-o", A])
-            
-            pcall([OPT, "-load=" + LLVMCONTECH, "-Contech", A, "-o", B, "-ContechState", stateFile])
-            # Compile bitcode back to a .o file
-            pcall([CC, CFLAGS, "-c", "-o", newobj, B])
-            # Add the generated object file to the list of things to link
-            ofiles = ofiles + " " + newobj
+        pcall(["gcc", CFLAGS, "-S", "-fplugin=" + CONTECH_LLVM_HOME + "/lib/dragonegg.so" , "-fplugin-arg-dragonegg-emit-ir", cfile, "-o", newobj])
+        ofiles = ofiles + " " + newobj
         
     # Input file found, assuming compile requested.
     # Compile with contech   
     elif cfile != "":
-        if True:
-            newobj = ""
-            if out != "" :
-                newobj = out
-            else:
-                # Get the name of cfile without an extension
-                if isCpp:
-                    name=cfile.replace(".cpp","",1);
-                    name=name.replace(".cc","",1);
-                else:
-                #linux.contech ... 
-                    name=cfile[0: len(cfile) - 2]
-                newobj = name + ".o"
-            if newobj[-2:] != ".o":
-                newobj = newobj + ".o"
-            pcall([CC, CFLAGS, cfile, "-emit-llvm", "-c", "-o", newobj])
-            ofiles = ofiles + " " + newobj
+        newobj = ""
+        if out != "" :
+            newobj = out
         else:
-        
             # Get the name of cfile without an extension
             if isCpp:
                 name=cfile.replace(".cpp","",1);
@@ -232,47 +207,11 @@ def main(isCpp = False, markOnly = False, minimal = False, hammer = False):
             else:
             #linux.contech ... 
                 name=cfile[0: len(cfile) - 2]
-            
-            # Define names of intermediate files
-            A= name + ".bc"
-            B= name + "_ct.bc"
-
-            # Define name of compiled file
-            newobj = ""
-            if out != "" :
-                newobj = out
-            else:
-                newobj = name + ".o"
-            
-            # Make sure the output ends in .o
-            if newobj[-2:] != ".o":
-                newobj = newobj + ".o"
-
-            # Compile with clang to emit LLVM bitcode
-            pcall([CC, CFLAGS, cfile, "-emit-llvm", "-c", "-o", A])
-            # Run the Contech pass to add instrumentation
-            
-            if markOnly:
-                pcall([OPT, "-load=" + LLVMCONTECH, "-Contech", A, "-o", B, "-ContechState", stateFile, "-ContechMarkFE"])
-            elif minimal:
-                pcall([OPT, "-load=" + LLVMCONTECH, "-Contech", A, "-o", B, "-ContechState", stateFile, "-ContechMinimal"])
-            elif hammer:
-                hammerNailFile = os.environ["HAMMER_NAIL_FILE"]
-                hammerOptLevel = os.environ["HAMMER_OPT_LEVEL"]
-                pcall([OPT, "-load=" + LLVMHAMMER, "-Hammer", A, "-o", B, "-HammerState", stateFile, "-HammerNailFile", hammerNailFile, "-HammerOptLevel", hammerOptLevel])
-            else:
-                if ARM == True:
-                    pcall([OPT, "-load=" + LLVMCONTECH, "-Contech", A, "-o", newobj, "-ContechState", stateFile])
-                else:
-                    pcall([OPT, "-load=" + LLVMCONTECH, "-Contech", A, "-o", B, "-ContechState", stateFile])
-            # Compile bitcode back to a .o file
-            if ARM == True:
-                print ""
-            else:
-                pcall([CC, CFLAGS, "-c", "-o", newobj, B])
-            # Add the generated object file to the list of things to link
-            ofiles = ofiles + " " + newobj
-        
+            newobj = name + ".o"
+        if newobj[-2:] != ".o":
+            newobj = newobj + ".o"
+        pcall([CC, CFLAGS, cfile, "-emit-llvm", "-c", "-o", newobj])
+        ofiles = ofiles + " " + newobj
     # Link 
     if not compileOnly:
         if ofiles != "":
@@ -280,65 +219,63 @@ def main(isCpp = False, markOnly = False, minimal = False, hammer = False):
             # Define name of final executable
             if out == "":
                 out = "a.out"
-                
-            if hammer:
-                # Compile final executable
-                pcall([CC, ofiles, CFLAGS, "-o", out, "-flto", "-lpthread", "-lz"])
+            # Does the binary use MPI?
+            if MPI == True:
+                RUNTIME = RUNTIME + CONTECH_HOME + "/common/runtime/ct_mpi.bc "
             else:
+                RUNTIME = RUNTIME + CONTECH_HOME + "/common/runtime/ct_nompi.bc "
                 
+            # Compile final executable
+
+            # But first extract .o files hiding in .a libraries
+            TMPARDIR = tempfile.mkdtemp()
+            if os.path.isdir(TMPARDIR):
+                pcall(["mkdir",  "-p", TMPARDIR])
+
+                remFlags = []
+                for flag in CFLAGS.split():
+                    if ".a" == flag[-2:]:
+                        aFilename = flag
+                        if not flag.find("/") == -1:
+                            aFilename = flag.split("/")[-1]
+                        pcall(["cp", flag, TMPARDIR])
+                        pcall(["cd", TMPARDIR, ";", CONTECH_HOME + "/scripts/uniq_ar_x.sh", aFilename])
+                        remFlags.append(flag)
+                for f in remFlags:
+                    print "Remove: " + f + "\t from: " + CFLAGS
+                    CFLAGS = CFLAGS.replace(f, "")
+                oFromA = glob.glob(TMPARDIR + "/*.o")
+                for f in oFromA:
+                    head = subprocess.Popen(["head", "-c", "3", f], stdout=subprocess.PIPE)
+                    oFromAType = head.stdout.read()
+                    if oFromAType.startswith("BC"):
+                        oFromAFiles = oFromAFiles + " " + f 
+
+                # As the result of link will be instrumented, do not include RUNTIME, etc
+                pcall(["llvm-link", ofiles, oFromAFiles, "-o", out + ".link.bc"])
+                pcall([OPT, "-load=" + LLVMCONTECH, "-Contech", out + ".link.bc", "-o", out + "_ct.link.bc", "-ContechState", stateFile])
+                pcall(["llvm-link", out + "_ct.link.bc", RUNTIME, "-o", out + "_ct.fin.bc"]);
+                pcall(["rm",  "-rf", TMPARDIR])
+
+            # Link in basic block table
+            shutil.copyfile(stateFile, "contech.bin")
+            # Note that we may have to create two .o, one for 32bit and one for 64bit
+            OBJCOPY = "objcopy"
+            
+            if ARM == True:
+                pcall([OBJCOPY, "--input binary", "--output elf32-littlearm", "--binary-architecture arm", "contech.bin", "contech_state.o"])
+            else:
+                pcall([OBJCOPY, "--input binary", "--output elf64-x86-64", "--binary-architecture i386", "contech.bin", "contech_state.o"])
+                #pcall([OBJCOPY, "--input binary", "--output elf32-i386", "--binary-architecture i386", "contech.bin", "contech_state.o"])
                 
-
-                # Does the binary use MPI?
-                if MPI == True:
-                    RUNTIME = RUNTIME + CONTECH_HOME + "/common/runtime/ct_mpi.bc "
-                else:
-                    RUNTIME = RUNTIME + CONTECH_HOME + "/common/runtime/ct_nompi.bc "
-                    
-                # Compile final executable
-
-                # But first extract .o files hiding in .a libraries
-                TMPARDIR = tempfile.mkdtemp()
-                if os.path.isdir(TMPARDIR):
-                    pcall(["mkdir",  "-p", TMPARDIR])
-
-                    for flag in CFLAGS.split():
-                        if ".a" == flag[-2:]:
-                            aFilename = flag
-                            if not flag.find("/") == -1:
-                                aFilename = flag.split("/")[-1]
-                            pcall(["cp", flag, TMPARDIR])
-                            pcall(["cd", TMPARDIR, ";", CONTECH_HOME + "/scripts/uniq_ar_x.sh", aFilename])
-                    oFromA = glob.glob(TMPARDIR + "/*.o")
-                    for f in oFromA:
-                        head = subprocess.Popen(["head", "-c", "3", f], stdout=subprocess.PIPE)
-                        oFromAType = head.stdout.read()
-                        if oFromAType.startswith("BC"):
-                            oFromAFiles = oFromAFiles + " " + f 
-
-                    # As the result of link will be instrumented, do not include RUNTIME, etc
-                    pcall(["llvm-link", ofiles, oFromAFiles, "-o", out + ".link.bc"])
-                    pcall([OPT, "-load=" + LLVMCONTECH, "-Contech", out + ".link.bc", "-o", out + "_ct.link.bc", "-ContechState", stateFile])
-                    pcall(["rm",  "-rf", TMPARDIR])
-
-                # Link in basic block table
-                shutil.copyfile(stateFile, "contech.bin")
-                # Note that we may have to create two .o, one for 32bit and one for 64bit
-                OBJCOPY = "objcopy"
-                
-                if ARM == True:
-                    pcall([OBJCOPY, "--input binary", "--output elf32-littlearm", "--binary-architecture arm", "contech.bin", "contech_state.o"])
-                else:
-                    pcall([OBJCOPY, "--input binary", "--output elf64-x86-64", "--binary-architecture i386", "contech.bin", "contech_state.o"])
-                    #pcall([OBJCOPY, "--input binary", "--output elf32-i386", "--binary-architecture i386", "contech.bin", "contech_state.o"])
-                    
-                if ARM == True:
-                    pcall([OPT, "-always-inline", out + "_ct.link.bc", "-o", out + "_ct_inline.bc"])
-                    pcall([CC, CFLAGS, "-c -o", out + "_ct.o", out + "_ct_inline.bc"])
-                    pcall([CC, out + "_ct.o", CFLAGS, "-o", out, "-lpthread", "contech_state.o"])
-                else:
-                    #Cilk runtime requires -ldl?
-                    #Contech runtime requires -lrt and -lpthread
-                    pcall([CC, out + "_ct.link.bc", RUNTIME, CFLAGS, "-o", out, "-lrt", "-ldl", "-flto", "-lpthread", "contech_state.o"])
+            if ARM == True:
+                pcall([OPT, "-always-inline", out + "_ct.link.bc", "-o", out + "_ct_inline.bc"])
+                pcall([CC, CFLAGS, "-c -o", out + "_ct.o", out + "_ct_inline.bc"])
+                pcall([CC, out + "_ct.o", CFLAGS, "-o", out, "-lpthread", "contech_state.o"])
+            else:
+                #Cilk runtime requires -ldl?
+                #Contech runtime requires -lrt and -lpthread
+                pcall([CC, out + "_ct.fin.bc", CFLAGS, "-o", out, "-lrt", "-ldl", "-lpthread",  "contech_state.o"])
         else:
             passThrough(CC)
 
